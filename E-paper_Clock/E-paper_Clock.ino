125836e06e928315f4b19a2b9773c4b5c2de13fd
j14273 <62167467+j14273@users.noreply.github.com>
2023-12-29 13:16:29 +1100

    FINAL

--- E-paper_Clock/E-paper_Clock.ino
+++ E-paper_Clock/E-paper_Clock.ino
@@ -13,59 +13,55 @@ RV3028 rtc;                       // create the RTC object
 
 const uint8_t wakeUpPin(2);  // connect Arduino pin D2 to RTC's SQW pin.
 
-const uint8_t changeFunc(4); // Button to change whether hours or minutes are changed and to turn off the change time mode
-const uint8_t changeOn(5); // Button to turn on changing time mode
-const uint8_t up(8); // Button to increase time
-const uint8_t down(3); // Button to decrease time
+const uint8_t changeFunc(4);  // Button to change whether hours or minutes are changed and to turn off the change time mode
+const uint8_t changeOn(5);    // Button to turn on changing time mode, this button causes the interrupt
+const uint8_t up(8);          // Button to increase time
+const uint8_t down(3);        // Button to decrease time
 
 
-//The below variables control what the date will be set to
+//The below variables control what the date will be set to. Uncomment the line bellow in setup that sets the rtc to the time here. After compiling, the line needs to be commented out again so that the Arduino does not set the time to this time every time it loses power
 int sec = 0;
-int minute = 44;
-int hour = 5;
-int day = 3;
-int date = 27;
+int minute = 52;
+int hour = 12;
+int day = 5;
+int date = 29;
 int month = 12;
 int year = 2023;
 
-volatile int changeTime = 0;
-    int changeFuncState = 0; //Function button set to 0 initally
+volatile int changeTime = 0; //Integer that gets changed when the interrupt occurs
+int changeFuncState = 0;  //Function button set to 0 initally
 
 void setup() {
-  int result;
   Serial.begin(115200);
   Wire.begin();
   if (rtc.begin() == false) {
     Serial.println("Something went wrong, check wiring");
-    while (1) 
-    return;
-    
+    while (1)
+      return;
   } else
     Serial.println("RTC online!");
   Serial.println();
 
   u8g2Fonts.begin(display);  // connect the u8g2
 
-
-  // configure an interrupt on the falling edge from SQN pin
+  // configures interupt pin
   pinMode(wakeUpPin, INPUT_PULLUP);
 
-// time adjustment buttons with pullup resistor
-  pinMode (changeOn, INPUT_PULLUP);
-  pinMode (changeFunc, INPUT_PULLUP);
-  pinMode (up, INPUT_PULLUP);
-  pinMode (down, INPUT_PULLUP);
+  // time adjustment buttons with pullup resistor
+  pinMode(changeOn, INPUT_PULLUP);
+  pinMode(changeFunc, INPUT_PULLUP);
+  pinMode(up, INPUT_PULLUP);
+  pinMode(down, INPUT_PULLUP);
 
-// allows for the change time buttons to interput the loop
-  PCICR |= B00000100; //turns on PCINT for pins in group d
-  PCMSK2 |= B00100000; //Pin D5 will interupt
-  rtc.enableTrickleCharge(TCR_3K);  //series resistor 3kOhm
-  rtc.setTime(sec, minute, hour, day, date, month, year);  //USE THIS TO INITALLY SET TIME. Once set it needs to be commented out
-  Serial.println("VOID SETUP = 1/2");
+  // allows for the change time buttons to interput the loop
+  PCICR |= B00000100;                                      //turns on PCINT for pins in group D
+  PCMSK2 |= B00100000;                                     //Pin D5 will cause interupt
+  rtc.enableTrickleCharge(TCR_3K);                         //series resistor 3kOhm
+  //rtc.setTime(sec, minute, hour, day, date, month, year);  //USE THIS TO INITALLY SET TIME. Once set it needs to be commented out so that it doesn't get set to this time every restart
   displayDate();
 }
 
-ISR (PCINT2_vect) {
+ISR(PCINT2_vect) {
   changeTime = 1;
 }
 
@@ -99,30 +95,26 @@ void displayDate() {
     u8g2Fonts.print(rtc.stringDate());
   } while (display.nextPage());
   display.hibernate();
-  Serial.println("DISPLAY DATE. = finished");
+  Serial.println("DISPLAY DATE = finished");
 }
 
 // Displays the time in the top half of the screen, as a partial refresh
 void displayTime() {
   Serial.println("DISPLAY TIME = start");
-  Serial.println("1 ");
   rtc.updateTime();
 
   u8g2Fonts.setForegroundColor(GxEPD_BLACK);
   u8g2Fonts.setBackgroundColor(GxEPD_WHITE);
   // Only numbers and symbols to save space.https://github.com/olikraus/u8g2/wiki/fntlist99#50-pixel-height
   u8g2Fonts.setFont(u8g2_font_logisoso50_tn);
-  Serial.print("4 ");
 
   uint16_t x = 60;
   uint16_t y = 62;  //top half, depends on font
-  Serial.print("5 ");
 
   display.setPartialWindow(0, 0, display.width(), display.height() / 2);
   display.firstPage();
   do  // Update the upper part of the screen
   {
-      Serial.print("6 ");
     u8g2Fonts.setCursor(x, y);
     if (rtc.getHours() < 10) {
       u8g2Fonts.print(0);
@@ -136,7 +128,6 @@ void displayTime() {
       u8g2Fonts.print(rtc.getMinutes());
     } else {
       u8g2Fonts.print(rtc.getMinutes());
-      //u8g2Fonts.print(timeString); This one has seconds which does not work on e-paper because of low refresh rate
     }
   } while (display.nextPage());
   display.hibernate();
@@ -145,7 +136,7 @@ void displayTime() {
 
 
 void loop() {
-  Serial.println("VOID LOOP = start");
+  Serial.println("LOOP = start");
 
   if (rtc.getMinutes() == 0) {
     // Refresh the display completely on the hour
@@ -154,9 +145,9 @@ void loop() {
 
   displayTime();
 
-  Serial.println("VOID LOOP = before sleep");
+  Serial.println("LOOP = before sleep");
 
-  delay(500);  // if this isn't here the arduino seems to fall asleep before finishing the last line
+  delay(500);
 
   rtc.enablePeriodicUpdateInterrupt(0, 0);
 
@@ -165,7 +156,7 @@ void loop() {
 
   Serial.println("VOID LOOP = powering down");
 
-  delay(500); // if this isn't here the arduino seems to fall asleep before finishing the last line
+  delay(500);  // if this isn't here the arduino seems to fall asleep before finishing the last line
 
   // Power down
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
@@ -176,6 +167,7 @@ void loop() {
   // Disable external pin interrupt on wake up pin.
   detachInterrupt(digitalPinToInterrupt(2));
 
+// Change time code
   while (changeTime == 1) {
     Serial.println("changetime");
     byte upState = digitalRead(up);
@@ -183,53 +175,48 @@ void loop() {
     digitalRead(changeFunc);
 
 
-  if ( digitalRead(changeFunc) == LOW ) { 
-    changeFuncState = changeFuncState + 1;
-    Serial.println(changeFuncState);
-    delay (250);
-  }
-
-  if ( changeFuncState == 0 ) { // Changes the hours. Runs when changeFuncState == 0, therefore runs right after the ISR and is the default mode.
-    if ( upState == LOW ) {
+    if (digitalRead(changeFunc) == LOW) {
+      changeFuncState = changeFuncState + 1;
+      Serial.println(changeFuncState);
       delay(250);
-      Serial.println("hr, up");
-      rtc.setHours(rtc.getHours() + 1);
-      displayTime();
-    }
-    else if ( downState == LOW ) {
-      delay(250);
-      Serial.println("hr, down");
-      rtc.setHours(rtc.getHours() - 1);
-      displayTime();
-    }
-  }
-
-  else if ( changeFuncState == 1 ) { // Changes the minutes
-    if ( upState == LOW ) {
-      delay(250);
-      Serial.println("min, up");
-      rtc.setMinutes(rtc.getMinutes() + 1);
-      displayTime();
-    }
-    else if ( downState == LOW ) {
-      delay(250);
-      Serial.println("min, down");
-      rtc.setMinutes(rtc.getMinutes() - 1);
-      displayTime();
     }
 
-  }
-  else if ( changeFuncState == 2 ) { //Turns off change time function of the clock
-  changeFuncState = 0;
-  changeTime = LOW;
-  displayDate(); //Fully refreshes the display
-  }
+    if (changeFuncState == 0) {  // Changes the hours. Runs when changeFuncState == 0, therefore runs right after the ISR and is the default mode.
+      if (upState == LOW) {
+        delay(250);
+        Serial.println("hr, up");
+        rtc.setHours(rtc.getHours() + 1);
+        displayTime();
+      } else if (downState == LOW) {
+        delay(250);
+        Serial.println("hr, down");
+        rtc.setHours(rtc.getHours() - 1);
+        displayTime();
+      }
+    }
 
-  } 
+    else if (changeFuncState == 1) {  // Changes the minutes
+      if (upState == LOW) {
+        delay(250);
+        Serial.println("min, up");
+        rtc.setMinutes(rtc.getMinutes() + 1);
+        displayTime();
+      } else if (downState == LOW) {
+        delay(250);
+        Serial.println("min, down");
+        rtc.setMinutes(rtc.getMinutes() - 1);
+        displayTime();
+      }
+
+    } else if (changeFuncState == 2) {  //Turns off change time function of the clock
+      changeFuncState = 0;
+      changeTime = LOW;
+      displayDate();  //Fully refreshes the display
+    }
+  }
 }
 
 
 
-void alarmIsr () {
-
+void alarmIsr() {
 }
